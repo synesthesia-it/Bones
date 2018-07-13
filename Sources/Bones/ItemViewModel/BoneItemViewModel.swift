4e3abaf17d1cc75a7253de9e71e025043f5b2502
Stefano Mondino <puntoste@gmail.com>
2018-07-13 14:55:35 +0200

    bonespec

--- Sources/Bones/ItemViewModel/BoneItemViewModel.swift
+++ Sources/Bones/ItemViewModel/BoneItemViewModel.swift
@@ -4,7 +4,7 @@ final class ___BonePlaceholder___ItemViewModel : ItemViewModelType {
     var model:ItemViewModelType.Model
     var itemIdentifier:ListIdentifier = View.___BonePlaceholderFirstLowercased___
     
-    init(model: _BonePlaceholder_) {
+    init(model: ___BonePlaceholder___) {
         self.model = model
     }
 }
