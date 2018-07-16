486a4dcc57ab79f004e45012faacab945fad20ce
Stefano Mondino <puntoste@gmail.com>
2018-07-16 15:58:05 +0200

    Model creation

---
+++ Sources/Bones/Model/Bone.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Gloss
+
+
+class ___BonePlaceholder___: Glossy {
+    var id:String
+    
+    public required init?(json: JSON) {
+        
+        guard let id: String = "id" <~~ json
+            else { return nil }
+        
+        self.id = id
+    }
+    
+    public func toJSON() -> JSON? {
+        return jsonify([
+            "id" ~~> id
+            ])
+    }
+  
+}
